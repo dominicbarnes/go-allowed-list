0c3135e4167bf5dde990895083efc6de6a642708
Dominic Barnes <dominic@dbarnes.info>
2016-12-08 19:46:43 -0800

    doc: add package introduction

--- interface.go
+++ interface.go
@@ -1,3 +1,5 @@
+// Package allowedList is a library for creating allowed lists using either the
+// blacklist (disallow items) and whitelist (only allow items) strategies.
 package allowedList
 
 // AllowedList is the shared API for both Blacklist and Whitelist.
